c530d713dff1a9a1c5f7b3b5ce701e92b6ebe3a7
Rohan Raja <rohanraja@Rohans-MacBook-Pro.local>
2012-12-28 02:29:46 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellow::Application.config.secret_token = '7d1825c1267c4a9f0e1311f57bd3d50b6e5048b78bca3a56d6f3841e163075ac2169331256b10486d16c036f1e8ce709c9a207e1ec91455fc7cc34d9dd0dd6b0'
